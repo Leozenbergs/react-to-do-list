6c0a92b70435795f220c0ce9783a487cd6ebbe39
Leonardo Rodrigues <leoabreu14@gmail.com>
2019-05-23 13:21:18 -0300

    Add Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Nav from './Nav';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,14 @@ class App extends Component {
     this.changeSub = this.changeSub.bind(this);
     this.removeNote = this.removeNote.bind(this);
     this.msgAlert = this.msgAlert.bind(this);
+    this.toggleNav = this.toggleNav.bind(this);
   }
   msgAlert(){
     document.getElementById('note_form').innerHTML+="<h3 style='color: red'> Please enter the respectives field values</h3>";
+  }
+  toggleNav(){
+    let nav = document.getElementById('nav');
+    nav.classList.contains('active')?nav.classList.remove('active') : nav.classList.add('active');
   }
   removeNote(e){
     var tgt = e.target;
@@ -22,14 +28,16 @@ class App extends Component {
     let title = document.getElementById('title').value;
     let message = document.getElementById('message').value;
 
-    title != '' && message != ''? document.getElementById('notes').innerHTML += `<div class="note_body"><div class="note_header">${title}<span class="pull-right c-pointer">&times;</span></div><div class="note_message">${message}</div></div>`: this.msgAlert();
+    title !== '' && message !== ''? document.getElementById('notes').innerHTML += `<div class="note_body"><div class="note_header">${title}<span class="pull-right c-pointer">&times;</span></div><div class="note_message">${message}</div></div>`: this.msgAlert();
     e.preventDefault();
   }
   componentDidMount(){
     document.addEventListener('click', (e)=>{
       this.removeNote(e);
     });
-    
+    document.getElementById('nav').addEventListener('click', ()=>{
+      this.toggleNav();
+    });
   }
   
 
@@ -37,7 +45,8 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-
+        <Nav onClick={this.toggleNav} />
+        <h2 className="text-center">Add notes</h2>
         <form className="main_form" id="note_form" onSubmit={this.changeSub}>
           <input type="text" id="title" name="title" placeholder="title"/>
           <textarea id="message" rows="4" placeholder="Message"></textarea>
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="side_nav" id="nav">
+            <ul>
+                <li><a href="">Recent</a></li>
+                <li><a href="">Lists</a></li>
+                <li><a href="">Favorites</a></li>
+            </ul>
+        </div>
+    );
+}
+export default Nav;
